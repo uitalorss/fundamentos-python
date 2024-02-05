66752db61b12849df5900ec5149e2cbebbbe2769
Uítalo <uitalo.urss@gmail.com>
2024-02-05 15:42:25 -0300

    configuração de ambiente e documentação

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+
+""" Hello Python
+
+Como usar:
+    Ter a variável LANG devidamente configurada
+        LANG=pt_BR
+
+Execução: 
+    python3 hello.py ou ./hello.py
+"""
+
+__version__ = "0.0.1"
+__author__ = "Uítalo Souza"
+__license__ = "Unlicense"
+
 print("hello")
